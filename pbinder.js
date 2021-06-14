5d680263694313c1cfd138173792138c0f357af4
xgao@zoyoe.com <xgao@zoyoe.com>
2021-06-14 23:27:21 +0800

    add promise binder trial

---
+++ pbinder.js
@@ -0,0 +1,38 @@
+class PromiseBinder {
+  constructor() {
+    this.actions = {snapshot:{}};
+  }
+  async bind (name, promise) {
+    let p = promise;
+    if (this.actions[name]) {
+      for (var c in this.actions[name]) {
+        p = p.on(c, this.actions[name][c]);
+        let v = await p;
+        return v;
+      }
+    } else {
+      let v = await p;
+      return v;
+    }
+  }
+  snapshot (name, func) {
+    if(this.actions[snapshot][name] != undefined) {
+      this.actions[snapshot][name]();
+    }
+  }
+  return (p) {
+    let promise = p();
+    promise.when = (action, c, fn) => {
+      if (this.actions[action] == undefined) {
+        this.actions[action] = {};
+      }
+      this.actions[action][c] = fn;
+      return promise;
+    };
+    return promise;
+  }
+}
+
+module.exports = {
+  PromiseBinder: PromiseBinder,
+}
