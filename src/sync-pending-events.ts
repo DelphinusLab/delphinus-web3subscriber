8cac6fff6569da372886390113cc49b4c378e6c7
RuilaiZhang <ray.zhangruilai@gmail.com>
2022-09-07 16:42:36 +1000

    update

--- src/sync-pending-events.ts
+++ src/sync-pending-events.ts
@@ -140,7 +140,6 @@ export class EventTracker {
     let latestBlockNumber = await getLatestBlockNumber(this.source);
     if(lastUpdatedBlock == 0){
       checkFromBlockNumber = zeroEventCheckedBlockNumber;
-      await db.updateZeroEventCheckedBlockNumber(latestBlockNumber);
     }else{
       checkFromBlockNumber = lastUpdatedBlock;
     }
@@ -161,6 +160,10 @@ export class EventTracker {
           await handlers(r.event, e, r.transactionHash);
           await db.updateLastMonitorBlock(r, e);
         }
+      }
+      lastUpdatedBlock = await db.getLastMonitorBlock();
+      if(lastUpdatedBlock == 0){
+        await db.updateZeroEventCheckedBlockNumber(latestBlockNumber);
       }
     } catch (err) {
       sendAlert(err, SlackConfig, true);
