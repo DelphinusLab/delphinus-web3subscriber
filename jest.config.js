db1f29743afe619bc6a9a7e8cb5a277d16cde6d1
RuilaiZhang <ray.zhangruilai@gmail.com>
2022-10-06 12:25:44 +1100

    modify comment

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ["**/?(*.)+(spec|test).[t]s?(x)"], //only match ts files and do not test the old invalid one
+    testMatch: ["**/?(*.)+(spec|test).[t]s?(x)"], //only match ts files
   };
