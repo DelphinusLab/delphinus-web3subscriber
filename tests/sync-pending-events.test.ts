9aa63b1c494ec160a8fcc78e465ee70c6654519a
RuilaiZhang <ray.zhangruilai@gmail.com>
2022-11-09 16:56:57 +1100

    modify unit test

--- tests/sync-pending-events.test.ts
+++ tests/sync-pending-events.test.ts
@@ -42,18 +42,25 @@ describe("test functions in syncEvent works", () => {
 
     test("test getReliableBlockNumber function works case 1", async () => {
         jest.setTimeout(60000); //1 minute timeout
+        //trueLatestBlockNumber:30, lastCheckedBlockNumber:10, bufferBlocks:10
+        //reliableBlockNumber = trueLatestBlockNumber - bufferBlocks = 10
         let reliableBlockNumber = await getReliableBlockNumber(30, 10, 10);
         expect(reliableBlockNumber).toEqual(20);
     });
 
     test("test getReliableBlockNumber function works case 2", async () => {
         jest.setTimeout(60000); //1 minute timeout
+        //trueLatestBlockNumber:10, lastCheckedBlockNumber:5, bufferBlocks:15
+        //trueLatestBlockNumber - bufferBlocks < 0
+        //reliableBlockNumber = lastCheckedBlockNumber 
         let reliableBlockNumber = await getReliableBlockNumber(10, 5, 15);
         expect(reliableBlockNumber).toEqual(5);
     });
 
     test("test getReliableBlockNumber function works case 3", async () => {
         jest.setTimeout(60000); //1 minute timeout
+        //trueLatestBlockNumber:null, lastCheckedBlockNumber:10, bufferBlocks:15
+        //reliableBlockNumber = lastCheckedBlockNumber
         let reliableBlockNumber = await getReliableBlockNumber(null, 10, 15);
         expect(reliableBlockNumber).toEqual(10);
     });
